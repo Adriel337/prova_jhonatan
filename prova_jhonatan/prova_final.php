4416ce44fd96068568022a0882c9b7218dfd3a56
Adriel337 <adrielmiguelduartedemelo@gmail.com>
2024-12-17 20:04:03 -0400

    Update prova_final.php

--- prova_jhonatan/prova_final.php
+++ prova_jhonatan/prova_final.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
     <title>Calculadora Multifuncional</title>
@@ -13,17 +12,7 @@
         <input type="submit" name="calcular_circulo" value="Calcular">
     </form>
 
-    <?php
-    if (isset($_POST['calcular_circulo'])) {
-        $raio = $_POST['raio'];
-        $perimetro = 2 * M_PI * $raio;
-        $area = M_PI * pow($raio, 2);
-        echo "Raio: $raio<br>";
-        echo "Perímetro: $perimetro<br>";
-        echo "Área: $area<br>";
-    }
-    ?>
-
+    
     <!-- Formulário para calcular a média de 3 notas -->
     <h2>Média de 3 Notas</h2>
     <form action="" method="post">
@@ -33,16 +22,7 @@
         <input type="submit" name="calcular_media" value="Calcular Média">
     </form>
 
-    <?php
-    if (isset($_POST['calcular_media'])) {
-        $nota1 = $_POST['nota1'];
-        $nota2 = $_POST['nota2'];
-        $nota3 = $_POST['nota3'];
-        $media = ($nota1 + $nota2 + $nota3) / 3;
-        echo "A média das notas é: $media<br>";
-    }
-    ?>
-
+    
     <!-- Formulário para converter Fahrenheit para Celsius -->
     <h2>Converter Fahrenheit para Celsius</h2>
     <form action="" method="post">
@@ -50,14 +30,7 @@
         <input type="submit" name="converter_para_celsius" value="Converter">
     </form>
 
-    <?php
-    if (isset($_POST['converter_para_celsius'])) {
-        $fahrenheit = $_POST['fahrenheit'];
-        $celsius = (5 * ($fahrenheit - 32)) / 9;
-        echo "A temperatura em Celsius é: $celsius<br>";
-    }
-    ?>
-
+    
     <!-- Formulário para converter Celsius para Fahrenheit -->
     <h2>Converter Celsius para Fahrenheit</h2>
     <form action="" method="post">
@@ -65,12 +38,5 @@
         <input type="submit" name="converter_para_fahrenheit" value="Converter">
     </form>
 
-    <?php
-    if (isset($_POST['converter_para_fahrenheit'])) {
-        $celsius = $_POST['celsius'];
-        $fahrenheit = ($celsius * 9/5) + 32;
-        echo "A temperatura em Fahrenheit é: $fahrenheit<br>";
-    }
-    ?>
-</body>
+    </body>
 </html>
